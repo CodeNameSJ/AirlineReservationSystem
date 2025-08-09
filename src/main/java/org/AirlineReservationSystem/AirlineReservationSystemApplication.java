076754411dc2e38fbe802c55828d86fe733d32e3
Shubham Jamdade <57455051+CodeNameSJ@users.noreply.github.com>
2025-08-10 01:15:30 +0530

    Update AirlineReservationSystemApplication.java

--- src/main/java/org/AirlineReservationSystem/AirlineReservationSystemApplication.java
+++ src/main/java/org/AirlineReservationSystem/AirlineReservationSystemApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AirlineReservationSystemApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AirlineReservationSystemApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineReservationSystemApplication.class, args);
+	}
 }
