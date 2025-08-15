0b8b88487975c139c2fd6ccde39c6574c7095e41
Shubham Jamdade <57455051+CodeNameSJ@users.noreply.github.com>
2025-08-16 02:18:21 +0530

    Update FlightRepository.java

--- src/main/java/org/AirlineReservationSystem/repository/FlightRepository.java
+++ src/main/java/org/AirlineReservationSystem/repository/FlightRepository.java
@@ -10,4 +10,16 @@ import java.util.List;
 @Repository
 public interface FlightRepository extends JpaRepository<Flight, Long> {
 	List<Flight> findByOriginAndDestinationAndDepartureTimeBetween(String origin, String destination, LocalDateTime start, LocalDateTime end);
+
+	List<Flight> findByDestinationAndDepartureTimeBetween(String destination, LocalDateTime start, LocalDateTime end);
+
+	List<Flight> findByDestination(String destination);
+
+	List<Flight> findByOrigin(String origin);
+
+	List<Flight> findByOriginAndDepartureTimeBetween(String origin, LocalDateTime start, LocalDateTime end);
+
+	List<Flight> findByOriginAndDestination(String origin, String destination);
+
+	List<Flight> findByDepartureTimeBetween(LocalDateTime start, LocalDateTime end);
 }
